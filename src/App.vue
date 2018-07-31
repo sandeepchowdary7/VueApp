19b67acc5a6960d79b306ff81c1f7388001cfe43
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-31 19:38:00 -0400

    VueApp init
    Adding new Vue-webpack dire

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+     <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {  msg: 'Welcome to My Vue App' }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
